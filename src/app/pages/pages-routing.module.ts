e8bbddc26d43204fae48649d767b6292436f640a
keerpugal <keerthipugal26111999@gmail.com>
2022-11-05 21:39:13 +0530

    workers page created

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkersComponent } from '../pages/workers/workers.component';
+import { WorkersListComponent } from './workers-list/workers-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:WorkersComponent},
+  {path:"workers",component:WorkersComponent},
+  {path:"workers-list",component:WorkersListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { WorkersListComponent } from './workers-list/workers-list.component';
+// import {MatPaginator} from '@angular/material/paginator';
+// import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WorkersListComponent
+    
+  ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule
+    // MatPaginator,
+    // MatTableDataSource
+    
   ]
 })
 export class PagesModule { }
