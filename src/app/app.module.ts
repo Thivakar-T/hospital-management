f66a39a79115706750d99317df687342303b6da0
sanjay <sanjaypyk@gmail.com>
2022-11-07 10:41:28 +0530

    doctor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from "ngx-spinner";
-<<<<<<< HEAD
+
 import { ReactiveFormsModule } from '@angular/forms';
-=======
-<<<<<<< HEAD
+
+
 import { NgWizardModule, NgWizardConfig, THEME } from 'ng-wizard';
 import { PatientformComponent } from './patientform/patientform.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -21,25 +21,24 @@ import { HttpClientModule } from '@angular/common/http';
 const ngWizardConfig: NgWizardConfig = {
   theme: THEME.default
 };
-=======
+
 import { NavbarComponent } from './../layout/navbar/navbar.component';
 import { FooterComponent } from './../layout/footer/footer.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatBadgeModule} from '@angular/material/badge';
 
->>>>>>> 862b2c122d6ee89d2be2558fbc1e80d13b65be62
->>>>>>> 43fbf58bbd377cb567b1fdeb7234b4ce26fc5de9
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+
   
-=======
-<<<<<<< HEAD
+
+
     PatientformComponent,
     PatientlistComponent
-=======
+
     NavbarComponent,
     FooterComponent,
 >>>>>>> 43fbf58bbd377cb567b1fdeb7234b4ce26fc5de9
@@ -58,9 +57,9 @@ import {MatBadgeModule} from '@angular/material/badge';
     ReactiveFormsModule,
     ToastrModule.forRoot(),
     NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
-<<<<<<< HEAD
+
     NgWizardModule.forRoot(ngWizardConfig)
-=======
+
     MatToolbarModule,
     MatBadgeModule,
     
