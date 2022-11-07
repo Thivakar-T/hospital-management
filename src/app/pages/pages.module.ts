8cc64a962b5c8caae2be90a5722dfc7c2c574fd2
keerpugal <keerthipugal26111999@gmail.com>
2022-11-07 11:51:50 +0530

    creation of worktable

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule ,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { WorkersListComponent } from './workers-list/workers-list.component';
- import {MatPaginator} from '@angular/material/paginator';
+//  import {MatPaginator} from '@angular/material/paginator';
 // import {MatTableDataSource} from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
